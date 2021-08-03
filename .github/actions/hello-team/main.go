47caeb27381684c8c4fd31b6a71cf8a0a6fd42e6
YuKyeongPark <21600280@handong.edu>
2021-08-03 20:43:27 +0900

    Modify Hello Team Action with input

--- .github/actions/hello-team/main.go
+++ .github/actions/hello-team/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main(){
-	fmt.Println("Hello YJ")
-	fmt.Println("Hello JH")
+	leader := os.Getenv("INPUT_LEADER")
+	member := os.Getenv("INPUT_MEMBER")
+
+	fmt.Println("Hello " + member)
+	fmt.Println("Hello " + leader)
 }
